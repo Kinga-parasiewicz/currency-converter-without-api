a2ecf68ce9e26de9c96918edd0cb33ebb1abb81e
kinga Parasiewicz <kingaparasiewicz@gmail.com>
2021-06-08 15:42:22 +0200

    added GlobalStyle and currencies

---
+++ src/currencies.js
@@ -0,0 +1,22 @@
+export const currencies = [
+    {
+        short: "EUR",
+        name: "Euro",
+        rate: 4.4660,
+    },
+    {
+        short: "GBP",
+        name: "Funt brytyjski",
+        rate: 4.8851,
+    },
+    {
+        short: "USD",
+        name: "Dolar amerykański",
+        rate: 3.9806,
+    },
+    {
+        short: "CHF",
+        name: "Frank szwajcarski",
+        rate: 4.1818,
+    },
+];
